17866cc563ca3d36bb27c4d9984d7dffb372111e
Damien Le Thiec <damien.lethiec@gmail.com>
2017-03-09 22:32:07 +0000

    add participation to schema (link between a user and a timetable)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309222356) do
+ActiveRecord::Schema.define(version: 20170309223046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170309222356) do
     t.string   "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.integer  "timetable_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["timetable_id"], name: "index_participations_on_timetable_id", using: :btree
+    t.index ["user_id"], name: "index_participations_on_user_id", using: :btree
   end
 
   create_table "timetables", force: :cascade do |t|
@@ -59,5 +68,7 @@ ActiveRecord::Schema.define(version: 20170309222356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "participations", "timetables"
+  add_foreign_key "participations", "users"
   add_foreign_key "timetables", "festivals"
 end
