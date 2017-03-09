dc47315a2bb50ef0526d2b7e72f75d70f16e8654
Damien Le Thiec <damien.lethiec@gmail.com>
2017-03-09 22:25:34 +0000

    add timetable model and migration

---
+++ app/models/timetable.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: timetables
+#
+#  id          :integer          not null, primary key
+#  festival_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_timetables_on_festival_id  (festival_id)
+#
+# Foreign Keys
+#
+#  fk_rails_e33000e2b1  (festival_id => festivals.id)
+#
+
+class Timetable < ApplicationRecord
+  belongs_to :festival
+end
