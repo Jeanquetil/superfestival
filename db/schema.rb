f233e7c7818333fdfe94096919b67fd1477ed0fa
Damien Le Thiec <damien.lethiec@gmail.com>
2017-03-09 22:24:46 +0000

    add festival model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309220508) do
+ActiveRecord::Schema.define(version: 20170309222356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170309220508) do
     t.string   "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "timetables", force: :cascade do |t|
+    t.integer  "festival_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["festival_id"], name: "index_timetables_on_festival_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +59,5 @@ ActiveRecord::Schema.define(version: 20170309220508) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "timetables", "festivals"
 end
