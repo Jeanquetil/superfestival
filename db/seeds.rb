9e1dd607d0f244704e46890554c03527f2129a91
Jean Anquetil <anquetiljean@gmail.com>
2017-04-22 18:27:29 +0200

    clean seeds and css utilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Festival.destroy_all
 
 Festival.create!(name: "Lollapalooza", start_at: Time.new(2016,9,10,0,0), end_at: Time.new(2016,9,11,0,0), location: "Berlin")
